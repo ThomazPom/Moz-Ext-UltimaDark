359881f0676c0ebf843637b98c8ca2645a6df52e
ThomazPom <thomas.benhamou@hotmail.fr>
2023-12-18 00:00:18 +0100

    Increased contentScript timer to 100ms

---
+++ content_script.js
@@ -0,0 +1,35 @@
+
+
+
+let myPort = browser.runtime.connect({name:"port-from-cs"});
+
+function csOverrideRemoteContent(url,content) {
+    myPort.postMessage({overrideRemoteContent:{ source:document.location.href, url:url, content:content }});
+};
+
+function resolveIDKVars(data) {
+    setTimeout(()=>{
+  let ikd_resolved = window.wrappedJSObject.uDark.edit_str(data.chunk,false,false,false,true);
+  
+//   console.log("In content script, resolving IDK vars: ", ikd_resolved,data.chunk);
+
+  data.chunk = ikd_resolved;
+  myPort.postMessage({resolvedIDKVars:data});
+//   console.log("In content script, resolved IDK vars: ", data);
+  setTimeout(()=>{
+    window.wrappedJSObject.uDark.refresh_stylesheet(data.details.url);
+},0)
+    }
+    ,100);
+}
+
+
+myPort.onMessage.addListener(function(m) {
+    // console.log("In content script, received message from background script: ",m);
+    m.havingIDKVars&& resolveIDKVars(m.havingIDKVars);
+});
+
+
+
+  
+exportFunction(csOverrideRemoteContent , window, { defineAs: "UDarkOverrideRemoteContent" });
