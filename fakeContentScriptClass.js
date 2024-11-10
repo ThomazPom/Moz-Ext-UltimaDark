9bcca90fb2e62e6d3f610c4e76274ff1f6a2a5cb
ThomazPom <thomas.benhamou@hotmail.fr>
2024-11-10 22:34:00 +0100

    New UltimaDark core

---
+++ fakeContentScriptClass.js
@@ -0,0 +1,7 @@
+// This file t.js serves two purposes:
+// 1. It avoids pollution of the uDarkExtendedBackground with content script specific code
+// 2. It allows the code mappiong and navigation of vscode to work properly
+
+class uDarkExtendedContentScript  {
+  
+}
