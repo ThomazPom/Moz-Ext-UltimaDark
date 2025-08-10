7734022a9ac7ffa8442d0a8961d25c1208dbf726
ThomazPom <thomas.benhamou@Hotmail.fr>
2025-08-10 03:04:22 +0200

    Update contentScriptClass.js

--- contentScriptClass.js
+++ contentScriptClass.js
@@ -83,14 +83,7 @@ class uDarkExtendedContentScript  {
        
       });
     }    
-    linkIntegrityErrorEvent = function(elem) {
-      // This fix is needed for some websites that use link integrity, i don't know why but sometime even removing the integrity earlier in the code does not work
-      uDark.info("Link integrity error", elem,  "lead to a reload of this script");
-      let href = elem.getAttribute("href");
-      href && elem.setAttribute("href",uDark.addNocacheToStrLink(href) );
-      elem.removeAttribute("onerror");
-      
-    }
+    
     override_website = function() {
       // Note : We dont support document.write() yet. Shoudl we find a way to monitor document.close?
       /*
@@ -127,7 +120,13 @@ class uDarkExtendedContentScript  {
       let start = performance.now();
       
       uDark.info( "Content script override website", window);
-      
+      linkIntegrityErrorEvent = function(elem) {
+      // This fix is needed for some websites that use link integrity, i don't know why but sometime even removing the integrity earlier in the code does not work
+        uDark.info("Link integrity error", elem,  "lead to a reload of this script");
+        let href = elem.getAttribute("href");
+        href && elem.setAttribute("href",uDark.addNocacheToStrLink(href) );
+        elem.removeAttribute("onerror");
+    }
       
       
       uDark.website_context = true; // Tell ultimadark we are in a website context and is_color_var is available
